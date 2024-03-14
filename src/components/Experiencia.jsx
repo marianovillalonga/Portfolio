1e114f7f89f454f3cb6174a2a1e32a129b95b817
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2024-03-14 16:14:57 -0300

    Mi experiancia

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,99 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const trabajos = [
-  {
-    id: 1,
-    titulo: 'Trabajo 1',
-    descripcion: 'Descripción del Trabajo 1...',
-    fechaInicio: 'Enero 2020',
-    fechaFin: 'Marzo 2021',
-  },
-  {
-    id: 2,
-    titulo: 'Trabajo 2',
-    descripcion: 'Descripción del Trabajo 2...',
-    fechaInicio: 'Abril 2021',
-    fechaFin: 'Julio 2022',
-  },
-  {
-    id: 3,
-    titulo: 'Trabajo 1',
-    descripcion: 'Descripción del Trabajo 1...',
-    fechaInicio: 'Enero 2020',
-    fechaFin: 'Marzo 2021',
-  },
-  {
-    id: 4,
-    titulo: 'Trabajo 2',
-    descripcion: 'Descripción del Trabajo 2...',
-    fechaInicio: 'Abril 2021',
-    fechaFin: 'Julio 2022',
-  },
-];
-
-const Card = styled.div`
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 20px;
-  margin-bottom: 20px;
-  max-width: 500px;
-  width: 100%;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease; /* Transición suave para el efecto de escala */
-  &:hover {
-    transform: scale(1.05); /* Aumenta el tamaño al 105% cuando se pasa el mouse por encima */
-  }
-`;
+import {
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardSubTitle,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Experiencia = () => {
   return (
-    <section style={styles.container}>
-      <h2 style={styles.title}>Mi Experiencia Laboral</h2>
-      <div style={styles.timeline}>
-        {trabajos.map((trabajo) => (
-          <Card key={trabajo.id}>
-            <h3 style={styles.titulo}>{trabajo.titulo}</h3>
-            <p style={styles.fecha}>{`${trabajo.fechaInicio} - ${trabajo.fechaFin}`}</p>
-            <p style={styles.descripcion}>{trabajo.descripcion}</p>
-          </Card>
-        ))}
+    <div className="text-center" style={{ marginTop: '1vh', marginBottom: '5vh' }}>
+      <h2>Mi experiencia laboral</h2>
+      <div className="d-flex justify-content-center align-items-center">
+        <MDBCard style={{ maxWidth: '400px', width: '100%' }}>
+          <MDBCardBody>
+            <MDBCardTitle>Cobromix </MDBCardTitle>
+            <MDBCardText>Herramientas utilizadas:</MDBCardText>
+            <MDBCardSubTitle>NodeJS, express, React, redux, MySql, Next.</MDBCardSubTitle>
+            <MDBBtn href='https://www.linkedin.com/company/cobromix/'>Linkedin de la empresa</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
       </div>
-    </section>
+      <div style={{ marginBottom: '20px' }}></div>
+      <div className="d-flex justify-content-center align-items-center">
+        <MDBCard style={{ maxWidth: '400px', width: '100%' }}>
+          <MDBCardBody>
+            <MDBCardTitle>Coderhouse </MDBCardTitle>
+            <MDBCardText>Herramientas utilizadas:</MDBCardText>
+            <MDBCardSubTitle>NodeJS, express, mongoDB, MySql.</MDBCardSubTitle>
+            <MDBBtn href='https://www.linkedin.com/school/coderhouse/'>Linkedin de la empresa</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    </div>
   );
-};
-
-const styles = {
-  container: {
-    padding: '100px 0',
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: '36px',
-    fontWeight: 'bold',
-    marginBottom: '30px',
-  },
-  timeline: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center', 
-  },
-  titulo: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    marginBottom: '10px',
-  },
-  fecha: {
-    fontSize: '16px',
-    marginBottom: '10px',
-    color: '#777',
-  },
-  descripcion: {
-    fontSize: '16px',
-    marginBottom: '10px',
-  },
 };
 
 export default Experiencia;
